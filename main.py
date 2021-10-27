770386b4be42b4a2fcbb6634d7bfb0bc83a4805e
Tania <shchennikova.tania@gmail.com>
2021-10-28 00:33:08 +0300

    Add Flask dependency, init main program

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask
+import pip
+
 
 def create_app():
-    app = Flask(name)
+    app = Flask(__name__)
 
     @app.route("/v1/hello-world-25")
     def hello():
         return "Hello World 25!"
 
     return app
-
